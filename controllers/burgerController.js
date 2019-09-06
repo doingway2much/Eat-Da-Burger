798a45bf00a4d98df3dec357237bbedf7ffbfb43
Seth Randell <sethhrandell@gmail.com>
2019-09-06 14:18:57 -0400

    Added an image of Whimpy to the jumbotron, created delete buttomm on devored burgers and made the burger images smaller

--- controllers/burgerController.js
+++ controllers/burgerController.js
@@ -20,7 +20,8 @@ router.post("/api/burgers", function(req, res) {
       req.body.name, 
     ], function(result) {
       // Send back the ID of the new quote
-      res.json({ id: result.insertId });
+      // res.json({ id: result.insertId });
+      res.redirect("/burgers");
     });
   });
 
@@ -33,6 +34,19 @@ router.post("/api/burgers", function(req, res) {
        devoured: "true"
     }, condition, function(result) {
       if (result.changedRows == 0) {
+        // If no rows were changed, then the ID must not exist, so 404
+        return res.status(404).end();
+      } else {
+        res.status(200).end();
+      }
+    });
+  });
+
+  router.delete("/api/burgers/:id", function(req, res) {
+    var condition = "id = " + req.params.id;
+  
+    burger.delete(condition, function(result) {
+      if (result.affectedRows == 0) {
         // If no rows were changed, then the ID must not exist, so 404
         return res.status(404).end();
       } else {
--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -38,5 +38,19 @@ $(function() {
           }
         );
       });
+      $(".undevour").on("click", function(event) {
+        var id = $(this).data("id");
+    
+        // Send the DELETE request.
+        $.ajax("/api/burgers/" + id, {
+          type: "DELETE"
+        }).then(
+          function() {
+            console.log("deleted burger", id);
+            // Reload the page to get the updated list
+            location.reload();
+          }
+        );
+      });
     });
     
