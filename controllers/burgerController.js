339af296cc2537014c30e26605fc989266a22d6e
Seth Randell <sethhrandell@gmail.com>
2019-09-06 15:54:59 -0400

    Added a redirect on the default rout to point to my burgers route

--- controllers/burgerController.js
+++ controllers/burgerController.js
@@ -2,6 +2,9 @@ var express = require("express");
 var router = express.Router();
 var burger = require("../models/burger.js");
 
+router.get("/", function(req, res) {
+  res.redirect("/burgers");
+});
 
 router.get("/burgers", function(req, res) {
     burger.all(function(data) {
